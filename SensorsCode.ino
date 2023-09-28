edaca6e2d321e090a83595045a93f4bd7540f6f7
Austin Luu <austinluu@prodigy.net>
2023-09-28 12:04:08 -0700

    Added functionality for on and off advertising when needed

--- SensorsCode.ino
+++ SensorsCode.ino
@@ -36,6 +36,7 @@ TaskHandle_t lightThread;
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 #define SUBSYSTEM_CHARACTERISTIC_UUID "cba1d466-344c-4be3-ab3f-189f80dd7518"
 #define DESCRIPTOR_UUID "00002902-0000-1000-8000-00805f9b34fb"
+#define SUBSYSTEM_DESCRIPTOR "3f0d768b-ddfb-4b46-acdb-e10b904ed065"
 
 #define NUM_LIGHT_READINGS 6
 #define NUM_SOUND_READINGS 5
@@ -48,6 +49,9 @@ TaskHandle_t lightThread;
 BluetoothSerial bluetooth;
 BLECharacteristic* characteristic;
 BLECharacteristic* subsystemCharacteristic;
+BLEAdvertising* advertising;
+
+int connectedDevices = 0;
 
 ////////////////////////////////////////////////////////////////////////////////////////
 // Pin setup for sensors
@@ -59,6 +63,36 @@ const int vibrationInputPin = 36;
 const int soundInputPin = 26;
 const int soundGatePin = 27;
 
+
+class MyServerCallbacks : public BLEServerCallbacks {
+  void onConnect(BLEServer* server) override;
+  void onDisconnect(BLEServer* server) override;
+};
+
+void MyServerCallbacks::onConnect(BLEServer* server) {
+  connectedDevices++;
+  if (advertising != nullptr) {
+     Serial.println("A device connected");
+     if (connectedDevices >= 1) {
+      Serial.println("All devices connected. Stopping BLE advertise...");
+      server->getAdvertising()->stop();
+     } else {
+      server->getAdvertising()->start(); 
+     }
+     Serial.print("Connected devices: ");
+     Serial.println(connectedDevices);
+  }
+}
+
+void MyServerCallbacks::onDisconnect(BLEServer* server) {
+  connectedDevices--;  
+  Serial.println("Device disconnected");
+  Serial.print("Connected devices: ");
+  Serial.println(connectedDevices);
+  if (connectedDevices < 1) {
+    server->getAdvertising()->start();
+  }
+}
 
 
 ////////////////////////////////////////////////////////////////////////////////////////
@@ -598,7 +632,6 @@ unsigned int minutes = 0;     // minutes that have passed since last detection
 ////////////////////////////////////////////////////////////////////////////////////////
 // Global boolean variable indicating the occupation status of the room
 ////////////////////////////////////////////////////////////////////////////////////////
-boolean isOccupied = false;
 
 ////////////////////////////////////////////////////////////////////////////////////////
 // JSON Object we will send to the Bluetooth app. It keeps track of the status as well as time since last detection
@@ -606,29 +639,54 @@ boolean isOccupied = false;
 StaticJsonDocument<300> jsonData;
 String stringData;
 
+
 ////////////////////////////////////////////////////////////////////////////////////////
 // BLE Response Class for parsing the response from the subsystem characteristic
 ////////////////////////////////////////////////////////////////////////////////////////
 class BLEResponse {
   private:
-  std::string response_;
+  std::string previousResponse = "";
+  boolean isOccupied = false;
+  
   public:
-  BLEResponse(std::string response) : response_(response) {}
-  void parseResponse() {
-    if (response_ == std::string("Occupied")) {
-      isOccupied = true;
-    } else {
-      isOccupied = false;
-    }
-  }
-  static void parseResponse(std::string response) {
+  void parseResponse(std::string response) {
     if (response == std::string("Occupied")) {
       isOccupied = true;
     } else {
       isOccupied = false;
     }
+    // When the room occupation status transitions, we want to reset the minutes counter
+    if (response != previousResponse) {
+      minutes = 0;
+      previousResponse = response;
+    }
   }
+  bool getOccupied() { return isOccupied; }
 };
+BLEResponse parser;
+////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
+class TempAdvertise {
+  public:
+  virtual void onTempAdvertising(int milliseconds) = 0;
+};
+
+class MyTempAdvertise: public TempAdvertise {
+  public:
+  void onTempAdvertising(int milliseconds) override;
+};
+
+void MyTempAdvertise::onTempAdvertising(int milliseconds) {
+  advertising->start();
+  delay(milliseconds);
+  advertising->stop();
+}
+
+void startTempAdvertising(TempAdvertise* tempAdvertise) {
+  tempAdvertise->onTempAdvertising(60000);
+}
+
+MyTempAdvertise* tempAdvertiseCallback = new MyTempAdvertise();
 ////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////
 
@@ -660,6 +718,8 @@ void vibrationTask(void* parameter) {
   }
 }
 
+
+
 void setup() {
   motionSensor = new MotionSensor(motionSensorInputPin);
   ultrasonicSensor = new UltrasonicSensor(trigPin, echoPin);
@@ -676,18 +736,23 @@ void setup() {
   Serial.println("Starting BLE setup...");
   BLEDevice::init("ESP32");
   BLEServer* server = BLEDevice::createServer();
+  server->setCallbacks(new MyServerCallbacks());
   BLEService* service = server->createService(SERVICE_UUID);
   characteristic = service->createCharacteristic(CHARACTERISTIC_UUID, BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE | BLECharacteristic::PROPERTY_NOTIFY | BLECharacteristic::PROPERTY_INDICATE);
-  subsystemCharacteristic = service->createCharacteristic(SUBSYSTEM_CHARACTERISTIC_UUID, BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE | BLECharacteristic::PROPERTY_NOTIFY | BLECharacteristic::PROPERTY_INDICATE);
+  subsystemCharacteristic = service->createCharacteristic(SUBSYSTEM_CHARACTERISTIC_UUID, BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE);
   subsystemCharacteristic->setValue("Occupied");
   characteristic->setValue("Test");
   BLEDescriptor descriptor(DESCRIPTOR_UUID);
   uint8_t descriptorValue[] = {0x00, 0x01};
+  BLEDescriptor subsystemDescriptor(SUBSYSTEM_DESCRIPTOR);
+  uint8_t subsystemDescriptorValue[] = {0x00, 0x01};
   descriptor.setValue(descriptorValue, 2);
+  subsystemDescriptor.setValue(subsystemDescriptorValue, 2);
+  subsystemCharacteristic->addDescriptor(&subsystemDescriptor);
   characteristic->addDescriptor(&descriptor);
   service->start();
 
-  BLEAdvertising* advertising = BLEDevice::getAdvertising();
+  advertising = BLEDevice::getAdvertising();
   advertising->addServiceUUID(SERVICE_UUID);
   advertising->setScanResponse(true);
   advertising->setMinPreferred(0x06);
@@ -719,13 +784,13 @@ void loop() {
   int activeSensors = motionSensor->isActive() + ultrasonicSensor->isActive() + lightSensor->isActive();
 
   // Read the subsystem characteristic value every loop. The String value could potentially change after a write from the subsystem
-  BLEResponse::parseResponse(subsystemCharacteristic->getValue());
+  parser.parseResponse(subsystemCharacteristic->getValue());
 
   // Boolean Data
   jsonData["motion"] = motionSensor->isActive();
   jsonData["proximity"] = ultrasonicSensor->isActive();
   jsonData["light"] = lightSensor->isActive();
-  jsonData["occupied"] = isOccupied;
+  jsonData["occupied"] = parser.getOccupied();
 
   // Numerical Data
   jsonData["lightIntensity"] = lightSensor->lightValue();
@@ -735,6 +800,8 @@ void loop() {
   jsonData["proximityBaseline"] = ultrasonicSensor->baseline();
   jsonData["lightBaseline"] = lightSensor->baseline();
   jsonData["lightOffBaseline"] = lightSensor->baseline_off();
+
+  jsonData["connected_devices"] = connectedDevices;
 
   if (lightSensor->lightValue() >= 120) {
     jsonData["lightingStatus"] = "Lights on";
@@ -794,6 +861,11 @@ void loop() {
       timeOfDetection = millis();
       minutes++;
       jsonData["lastDetected"] = minutes;
+      if (minutes >= 2 && parser.getOccupied()) {
+        // Start advertising and send an alert to the client device
+        startTempAdvertising(tempAdvertiseCallback);
+      }
+
       serializeJson(jsonData, stringData);
       sendBluetoothMessage(stringData, characteristic);
     }
